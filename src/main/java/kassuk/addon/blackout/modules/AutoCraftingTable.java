f6a62d427d9aa69c90476b8da5ef167790d24545
GhostTypes <106415648+GhostTypes@users.noreply.github.com>
2026-08-10 23:17:52 -0400

    chore: drop stale IDE-remapper junk comments from 7 modules
    Removes pre-existing '// TODO(Ravel): Failed to fully resolve file...'
    line-1 debris carried over from the abandoned original's IntelliJ
    remapper (AutoCraftingTable, AutoTrapPlus, Blocker, PistonCrystal,
    PistonPush, SelfTrapPlus, SurroundPlus). Comment-only; no semantic
    change; compileJava still green.

--- src/main/java/kassuk/addon/blackout/modules/AutoCraftingTable.java
+++ src/main/java/kassuk/addon/blackout/modules/AutoCraftingTable.java
@@ -1,4 +1,3 @@
-// TODO(Ravel): Failed to fully resolve file: null cannot be cast to non-null type com.intellij.psi.PsiJavaCodeReferenceElement
 package kassuk.addon.blackout.modules;
 
 import kassuk.addon.blackout.BlackOut;
